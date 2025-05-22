06f3e503a44c63d92cd57d072b831ab36b4eb8d0
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2025-05-22 12:21:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UnitX-XX-Python
+[![Mr Coxall's Super Linter](README.md/../../../workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](README.md/../../../actions)
+
---
+++ file_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: Month DayNum, YEAR
+# DESCRIPTION HERE
+
+
+def main():
+    # Code goes here
+    pass
+
+
+if __name__ == "__main__":
+    main()
